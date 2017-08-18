621038af18767d1cfd1be2db9fedd9a8b808a9de
Dariana <dari_p@msn.com>
2017-08-18 17:15:05 +0200

    Commit 18/08 17

---
+++ src/test/java/com/everis/alicante/becajava/services/implementaciones/ClientServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.everis.alicante.becajava.services.implementaciones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientServiceImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
